shankaraner/perl5

POSIX.xs: White space only
Vertically align for readability

ext/POSIX/POSIX.xs
@@ -1598,8 +1598,8 @@ static const struct lconv_offset lconv_strings[] = {
 
 /* The Linux man pages say these are the field names for the structure
  * components that are LC_NUMERIC; the rest being LC_MONETARY */
-#   define isLC_NUMERIC_STRING(name) (strEQ(name, "decimal_point")     \
-                                      || strEQ(name, "thousands_sep")  \
+#   define isLC_NUMERIC_STRING(name) (   strEQ(name, "decimal_point")   \
+                                      || strEQ(name, "thousands_sep")   \
                                                                         \
                                       /* There should be no harm done   \
                                        * checking for this, even if     \

opt9/cperl

(cherry picked from commit 93e25eee77b400afab2c5cb9ad09a5c79ada1eda)

ext/POSIX/POSIX.xs
@@ -1604,8 +1604,8 @@ static const struct lconv_offset lconv_strings[] = {
 
 /* The Linux man pages say these are the field names for the structure
  * components that are LC_NUMERIC; the rest being LC_MONETARY */
-#   define isLC_NUMERIC_STRING(name) (strEQc(name, "decimal_point")     \
-                                      || strEQc(name, "thousands_sep")  \
+#   define isLC_NUMERIC_STRING(name) (   strEQc(name, "decimal_point")   \
+                                      || strEQc(name, "thousands_sep")   \
                                                                         \
                                       /* There should be no harm done   \
                                        * checking for this, even if     \